e8a543cc97b5b332183e4ace3907483b3eed5162
bacolenke <bacolenke@gmail.com>
2026-05-26 01:25:00 +0300

    Match header class names to current CSS

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -17,18 +17,18 @@ export function SiteHeader() {
           <div className="menu-items">
             {items.map((item) => (
               <a href="#home" className="menu-item toggle-item" key={item}>
-                <span className="text-wrap">
-                  <span className="text-white">{item}</span>
-                  <span className="text-gradient">{item}</span>
+                <span className="menu-text-wrap">
+                  <span className="menu-text-white">{item}</span>
+                  <span className="menu-text-gradient">{item}</span>
                 </span>
               </a>
             ))}
           </div>
         </nav>
         <a href="#home" className="menu-item menu-contact">
-          <span className="text-wrap">
-            <span className="text-white">Get In Touch</span>
-            <span className="text-gradient">Get In Touch</span>
+          <span className="menu-text-wrap">
+            <span className="menu-text-white">Get In Touch</span>
+            <span className="menu-text-gradient">Get In Touch</span>
           </span>
         </a>
       </div>
